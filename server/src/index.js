6850b1e91521713cd6e9d67e1e9502eed6a38ac0
Jacob Clevenger <jacobrclevenger@gmail.com>
2019-01-28 20:41:28 -0500

    Updated PORT

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
 
-const PORT = 3000;
+const PORT = 8320;
 const HOST = 'localhost';
 const DATA_DIRECTORY = path.join(__dirname, `../../core/data`);
 
